afc77a779cee9d46086953d38708c58074bf0da0
Jo Egner <14022395+vechema@users.noreply.github.com>
2021-11-18 15:58:39 -0600

    Pretty up claims digging

--- lib/change_health/response/claim/report/report_277_data.rb
+++ lib/change_health/response/claim/report/report_277_data.rb
@@ -4,7 +4,7 @@ module ChangeHealth
       # Methods only return meaningful data for json reports
       class Report277Data < ChangeHealth::Response::Claim::ReportData
         def transactions
-          @raw.dig('transactions')
+          @raw['transactions']
         end
 
         # Only one payer per report
@@ -16,32 +16,34 @@ module ChangeHealth
           report_claims = []
 
           transactions&.each do |transaction|
-            transaction_set_creation_date = ChangeHealth::Models::PARSE_DATE.call(transaction.dig('transactionSetCreationDate'))
-            transaction.dig('payers')&.each do |payer|
-              payer_org_name = payer.dig('organizationName')
-              payer.dig('claimStatusTransactions')&.each do |claim_status_txn|
-                claim_status_txn.dig('claimStatusDetails')&.each do |claim_status_detail|
-                  service_provider_npi = claim_status_detail.dig('serviceProvider')&.dig('npi')
-                  claim_status_detail.dig('patientClaimStatusDetails')&.each do |patient_claim_status_detail|
-                    subscriber_first_name = patient_claim_status_detail.dig('subscriber')&.dig('firstName')
-                    subscriber_last_name = patient_claim_status_detail.dig('subscriber')&.dig('lastName')
-                    patient_claim_status_detail.dig('claims')&.each do |claim|
+            transaction_set_creation_date = ChangeHealth::Models::PARSE_DATE.call(transaction['transactionSetCreationDate'])
+            transaction['payers']&.each do |payer|
+              payer_org_name = payer['organizationName']
+              payer['claimStatusTransactions']&.each do |claim_status_txn|
+                claim_status_txn['claimStatusDetails']&.each do |claim_status_detail|
+                  service_provider_npi = claim_status_detail.dig('serviceProvider', 'npi')
+                  claim_status_detail['patientClaimStatusDetails']&.each do |patient_claim_status_detail|
+                    subscriber_first_name = patient_claim_status_detail.dig('subscriber', 'firstName')
+                    subscriber_last_name = patient_claim_status_detail.dig('subscriber', 'lastName')
+                    patient_claim_status_detail['claims']&.each do |claim|
                       procedure_codes = []
-                      claim.dig('serviceLines')&.each do |service_line|
-                        procedure_codes << service_line.dig('service').dig('procedureCode')
+                      claim['serviceLines']&.each do |service_line|
+                        procedure_codes << service_line.dig('service', 'procedureCode')
                       end
-                      claim_status = claim.dig('claimStatus')
-                      service_begin_date = ChangeHealth::Models::PARSE_DATE.call(claim_status&.dig('claimServiceBeginDate') || claim_status&.dig('claimServiceDate'))
-                      service_end_date = ChangeHealth::Models::PARSE_DATE.call(claim_status&.dig('claimServiceEndDate') || claim_status&.dig('claimServiceDate'))
+                      claim_status = claim['claimStatus']
+                      next if claim_status.nil?
+
+                      service_begin_date = ChangeHealth::Models::PARSE_DATE.call(claim_status['claimServiceBeginDate'] || claim_status['claimServiceDate'])
+                      service_end_date = ChangeHealth::Models::PARSE_DATE.call(claim_status['claimServiceEndDate'] || claim_status['claimServiceDate'])
 
                       info_claim_statuses = []
-                      claim_status&.dig('informationClaimStatuses')&.each do |info_claim_status|
-                        status_information_effective_date = ChangeHealth::Models::PARSE_DATE.call(info_claim_status.dig('statusInformationEffectiveDate'))
-                        total_charge_amount = info_claim_status.dig('totalClaimChargeAmount')
+                      claim_status['informationClaimStatuses']&.each do |info_claim_status|
+                        status_information_effective_date = ChangeHealth::Models::PARSE_DATE.call(info_claim_status['statusInformationEffectiveDate'])
+                        total_charge_amount = info_claim_status['totalClaimChargeAmount']
 
                         status_category_codes = []
-                        info_claim_status.dig('informationStatuses')&.each do |info_status|
-                          status_category_codes << info_status.dig('healthCareClaimStatusCategoryCode')
+                        info_claim_status['informationStatuses']&.each do |info_status|
+                          status_category_codes << info_status['healthCareClaimStatusCategoryCode']
                         end
 
                         info_claim_statuses << ReportInfoClaimStatus.new(
