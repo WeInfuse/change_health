afdb1e31db0c27b056a79f0b628c1250616e7a97
Deborah Seow <deborah@Deborahs-MBP.lan>
2022-04-07 19:25:08 -0500

    Moved line_adjudication work to change_health gem

--- lib/change_health/response/claim/report/report_835_service_line.rb
+++ lib/change_health/response/claim/report/report_835_service_line.rb
@@ -8,6 +8,46 @@ module ChangeHealth
         property :line_item_charge_amount, required: false
         property :line_item_provider_payment_amount, required: false
         property :service_adjustments, required: false
+
+        def create_group_adjustments(service_adjustments)
+          adjustment_array = service_adjustments[:adjustments].map do |key, value|
+            {
+              adjustmentReasonCode: key,
+              adjustmentAmount: value
+            }
+          end
+          {
+            adjustmentDetails: adjustment_array,
+            adjustmentGroupCode: service_adjustments[:claim_adjustment_group_code]
+          }
+        end
+
+        def create_remark_code_adjustments(remark_codes_array)
+          adjustment_array = remark_codes_array.map do |_key, value|
+            {
+              adjustmentReasonCode: value,
+              adjustmentAmount: ""
+            }
+          end
+          {
+            adjustmentDetails: adjustment_array,
+            adjustmentGroupCode: ""
+          }
+        end
+
+        def create_adjustment_detail_array
+          adjustment_details = []
+          all_service_adjustments = self.service_adjustments
+          all_service_adjustments.each do |service_adjustments|
+            adjustment_details << create_group_adjustments(service_adjustments)
+          end
+
+          health_care_check_remark_codes = self[:health_care_check_remark_codes]
+          health_care_check_remark_codes.each do |remark_codes|
+            adjustment_details << create_remark_code_adjustments(remark_codes)
+          end
+          adjustment_details
+        end
       end
     end
   end
