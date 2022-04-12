66e370c2443f6c137579a3529e2e44db47065992
Deborah Seow <deborah@Deborahs-MBP.lan>
2022-04-12 12:30:21 -0500

    Added things I forgot

--- lib/change_health/models/claim/submission/service_line.rb
+++ lib/change_health/models/claim/submission/service_line.rb
@@ -6,6 +6,7 @@ module ChangeHealth
         property :serviceDate, from: :service_date, required: false
         property :professionalService, from: :professional_service, required: false
         property :renderingProvider, from: :rendering_provider, required: false
+        property :line_adjudication_information, from: :line_adjudication_information, required: false
       end
     end
   end
