5446a3d9e14bd5eaaf4659e31ad32b98a7c82726
Jo Egner <14022395+vechema@users.noreply.github.com>
2022-12-01 09:08:46 -0600

    183866185 Can pull any report type

--- lib/change_health/request/report.rb
+++ lib/change_health/request/report.rb
@@ -7,29 +7,41 @@ module ChangeHealth
 
         def self.report_list(headers: nil)
           final_headers = ChangeHealth::Request::Claim::Report.report_headers(headers)
-          ChangeHealth::Response::Claim::ReportListData.new(response: ChangeHealth::Connection.new.request(endpoint: ENDPOINT, verb: :get, headers: final_headers))
+          ChangeHealth::Response::Claim::ReportListData.new(response: ChangeHealth::Connection.new.request(
+            endpoint: ENDPOINT, verb: :get, headers: final_headers
+          ))
         end
 
         def self.get_report(report_name, as_json_report: true, headers: nil)
           return if report_name.nil? || report_name.empty?
-          final_headers =  ChangeHealth::Request::Claim::Report.report_headers(headers)
 
-          report_type = ChangeHealth::Response::Claim::ReportData.report_type(report_name)
-          return if report_type.nil?
+          final_headers = ChangeHealth::Request::Claim::Report.report_headers(headers)
 
-          individual_report_endpoint = ENDPOINT + '/' + report_name
+          individual_report_endpoint = "#{ENDPOINT}/#{report_name}"
 
           # https://developers.changehealthcare.com/eligibilityandclaims/docs/what-file-types-does-this-api-get-from-the-mailbox
-          individual_report_endpoint += '/' + report_type if as_json_report
+          if as_json_report
+            report_type = ChangeHealth::Response::Claim::ReportData.report_type(report_name)
+            individual_report_endpoint += "/#{report_type}"
+          end
 
-          response = ChangeHealth::Connection.new.request(endpoint: individual_report_endpoint, verb: :get, headers: final_headers)
+          response = ChangeHealth::Connection.new.request(
+            endpoint: individual_report_endpoint,
+            verb: :get,
+            headers: final_headers
+          )
           if ChangeHealth::Response::Claim::ReportData.is_277?(report_name)
             ChangeHealth::Response::Claim::Report277Data
               .new(report_name,
                    as_json_report,
                    response: response)
-          else
+          elsif ChangeHealth::Response::Claim::ReportData.is_835?(report_name)
             ChangeHealth::Response::Claim::Report835Data
+              .new(report_name,
+                   as_json_report,
+                   response: response)
+          else
+            ChangeHealth::Response::Claim::ReportData
               .new(report_name,
                    as_json_report,
                    response: response)
@@ -41,17 +53,15 @@ module ChangeHealth
         end
 
         def self.ping
-          self.health_check
+          health_check
         end
 
         def self.report_headers(headers)
           if headers
             extra_headers = {}
-            extra_headers["X-CHC-Reports-Username"] = headers[:username]
-            extra_headers["X-CHC-Reports-Password"] = headers[:password]
+            extra_headers['X-CHC-Reports-Username'] = headers[:username]
+            extra_headers['X-CHC-Reports-Password'] = headers[:password]
             extra_headers
-          else
-            nil
           end
         end
       end
