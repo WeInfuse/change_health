cc70d2f5b70b0f35fd03a72812ccb805df428621
cjangam <chandra.jangam@lendstreet.com>
2023-01-13 19:15:00 +0530

    raw_service_lines added

--- lib/change_health/response/claim/report/report_835_claim.rb
+++ lib/change_health/response/claim/report/report_835_claim.rb
@@ -9,6 +9,7 @@ module ChangeHealth
         property :patient_control_number, required: false
         property :payer_claim_control_number, required: false
         property :service_lines, required: false
+        property :raw_service_lines, required: false
         property :total_charge_amount, required: false
 
         def procedure_codes
