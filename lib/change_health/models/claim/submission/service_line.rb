fe6e6ab5e12bb558812e9660b4f616fe5f7e5375
cjangam <chandra.jangam@lendstreet.com>
2023-07-04 13:47:41 +0530

    Add providerControlNumber to service line

--- lib/change_health/models/claim/submission/service_line.rb
+++ lib/change_health/models/claim/submission/service_line.rb
@@ -4,6 +4,7 @@ module ChangeHealth
       class ServiceLine < Model
         property :drugIdentification, from: :drug_identification, required: false
         property :serviceDate, from: :service_date, required: false
+        property :providerControlNumber, from: :provider_control_number, required: false
         property :professionalService, from: :professional_service, required: false
         property :renderingProvider, from: :rendering_provider, required: false
         property :procedureModifiers, from: :procedure_modifiers, required: false
