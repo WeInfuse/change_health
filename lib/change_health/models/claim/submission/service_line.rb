aafa8d7732c49460797c6ed0a58b950255dc40c7
Deborah Seow <26316820+AkaKuro4Ever@users.noreply.github.com>
2023-10-30 14:49:35 -0500

    Added all changes

--- lib/change_health/models/claim/submission/service_line.rb
+++ lib/change_health/models/claim/submission/service_line.rb
@@ -9,6 +9,7 @@ module ChangeHealth
         property :professionalService, from: :professional_service
         property :renderingProvider, from: :rendering_provider
         property :serviceDate, from: :service_date
+        property :serviceDateEnd, from: :service_date_end, required: false
       end
     end
   end
